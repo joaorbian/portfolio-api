2dc4a4c52ed910d61b2446241405de3f0af2ae60
progjoao <joaoricardobianchini@hotmail.com>
2023-01-28 20:36:38 -0300

    change description varchar to longtext

--- src/models/ProjectModel.ts
+++ src/models/ProjectModel.ts
@@ -7,7 +7,7 @@ export async function createProjectTable() {
       created_at DATETIME NOT NULL,
       updated_at DATETIME NOT NULL,
       title varchar(255) NOT NULL,
-      description varchar(255) NOT NULL,
+      description longtext NOT NULL,
       website varchar(255) NOT NULL,
       git varchar(255) NOT NULL)
   `);
